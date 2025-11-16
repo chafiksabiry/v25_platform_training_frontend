4f01af9e6ffd6ebde076f72ecddef208beb178ff
El Houcine Qara <elhoucineqara114@gmail.com>
2025-11-16 18:22:39 +0100

    Update

--- src/components/DocumentViewer/DocumentViewer.tsx
+++ src/components/DocumentViewer/DocumentViewer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { FileText, Download, ExternalLink } from "lucide-react";
 
 interface DocumentViewerProps {
