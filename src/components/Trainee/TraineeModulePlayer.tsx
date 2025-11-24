23eb586a5216a38838742890c602d5720c0da52f
El Houcine Qara <elhoucineqara114@gmail.com>
2025-11-24 01:20:33 +0100

    Update

--- src/components/Trainee/TraineeModulePlayer.tsx
+++ src/components/Trainee/TraineeModulePlayer.tsx
@@ -1084,8 +1084,8 @@ export default function TraineeModulePlayer({
             </div>
           )}
 
-          {/* Regular Quiz Modal (for inline quizzes during module) */}
-          {!showModuleQuiz && currentQuiz && (
+          {/* Regular Quiz Modal (for inline quizzes during module) - DISABLED */}
+          {false && !showModuleQuiz && currentQuiz && (
             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
               <div className="bg-white rounded-lg max-w-md w-full">
                 <div className="p-6 border-b border-gray-200">
